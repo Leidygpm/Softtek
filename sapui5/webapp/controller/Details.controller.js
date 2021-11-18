aadedd46352f95e8a1eab01a62e4a0a77665dc2b
Leidy Giselle Parra Mora <leidy.parra@softtek.com>
2021-11-18 01:52:21 +0000

    Demo v3

--- sapui5/webapp/controller/Details.controller.js
+++ sapui5/webapp/controller/Details.controller.js
@@ -2,7 +2,7 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/routing/History",
 ],
-
+//Detalle
     /**
          * @param {typeof sap.ui.core.mvc.Controller} Controller
          * @param {typeof sap.ui.core.routing.History} History
